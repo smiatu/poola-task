8ed97934086b3670858201a99de822c84908ffbb
smiatu <psmiatek@gmail.com>
2023-03-08 00:59:07 +0100

    Create Personal Data panel

--- tasks1/src/App.tsx
+++ tasks1/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Search from './components/Search/Search';
+import Panel from './components/Panel/Panel';
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Search searchType={'space'}/>
       <Search searchType={'address'} />
+      <Panel />
     </div>
   );
 }
--- tasks1/src/service/search.test.js
+++ tasks1/src/service/search.test.js
@@ -1,5 +1,5 @@
 import { searchSpaces } from './search'
-import { delay } from "./common";
+import { delay, delayedSearch } from './common'
 
 describe("delay", () => {
     it("should wait for the specified time before resolving the promise", async () => {
@@ -13,10 +13,35 @@ describe("delay", () => {
 
 describe("searchSpaces with delay", () => {
     it("should return non empty array or error after a delay", async () => {
-        const results = await searchSpaces("")
-            .then(delay(500))
-            .then((a) => a.spaces);
-        expect(results).toBeInstanceOf(Array || Error);
-        expect(results.length).toBeGreaterThanOrEqual(1);
+        let results;
+        try {
+            results = await searchSpaces("")
+                .then(delay(500))
+                .then((a) => a.spaces);
+            expect(results).toBeInstanceOf(Array);
+            expect(results.length).toBeGreaterThanOrEqual(1);
+        } catch (error) {
+            expect(error).toBeInstanceOf(Error);
+            expect(error.message).toEqual("Network error");
+        }
+    });
+});
+
+describe("delayedSearch", () => {
+    it("should work similar to searchSpaces with delay", async () => {
+        let results;
+        const startTime = new Date().getTime();
+        try {
+            results = await delayedSearch("", 500, 'space')
+                .then((a) => a.spaces);
+            const endTime = new Date().getTime();
+            const elapsedTime = endTime - startTime;
+            expect(elapsedTime).toBeGreaterThanOrEqual(500);
+            expect(results).toBeInstanceOf(Array);
+            expect(results.length).toBeGreaterThanOrEqual(1);
+        } catch (error) {
+            expect(error).toBeInstanceOf(Error);
+            expect(error.message).toEqual("Network error");
+        }
     });
 });
